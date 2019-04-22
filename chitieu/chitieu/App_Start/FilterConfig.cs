b694920e551956af1cc8e3103d9f34a4a60d42ac
xuanhoang2017 <48082709+xuanhoang2017@users.noreply.github.com>
2019-04-22 11:00:49 +0700

    data and controller

---
+++ chitieu/chitieu/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace chitieu
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ chitieu/chitieu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chitieu.MvcApplication" Language="C#" %>
