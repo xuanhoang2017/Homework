08ce21327725506d6e7fd2bfc20efb51121407f9
xuanhoang2017 <48082709+xuanhoang2017@users.noreply.github.com>
2019-04-22 11:12:54 +0700

    edit detail....

--- chitieu/chitieu/Controllers/expenditureController.cs
+++ chitieu/chitieu/Controllers/expenditureController.cs
@@ -15,12 +15,122 @@ namespace chitieu.Controllers
         private AnchoiEntities db = new AnchoiEntities();
 
         // GET: /expenditure/
+//        public ActionResult Index()
+//        {
+//            return View(db.expenditures.ToList());
+//        }
+
+//        // GET: /expenditure/Details/5
+//        public ActionResult Details(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            expenditure expenditure = db.expenditures.Find(id);
+//            if (expenditure == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(expenditure);
+//        }
+
+//        // GET: /expenditure/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
+
+//        // POST: /expenditure/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include="id,datetime,amount,note")] expenditure expenditure)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.expenditures.Add(expenditure);
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+
+//            return View(expenditure);
+//        }
+
+//        // GET: /expenditure/Edit/5
+//        public ActionResult Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            expenditure expenditure = db.expenditures.Find(id);
+//            if (expenditure == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(expenditure);
+//        }
+
+//        // POST: /expenditure/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include="id,datetime,amount,note")] expenditure expenditure)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Entry(expenditure).State = EntityState.Modified;
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return View(expenditure);
+//        }
+
+//        // GET: /expenditure/Delete/5
+//        public ActionResult Delete(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            expenditure expenditure = db.expenditures.Find(id);
+//            if (expenditure == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(expenditure);
+//        }
+
+//        // POST: /expenditure/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(int id)
+//        {
+//            expenditure expenditure = db.expenditures.Find(id);
+//            db.expenditures.Remove(expenditure);
+//            db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
         public ActionResult Index()
         {
             return View(db.expenditures.ToList());
         }
 
-        // GET: /expenditure/Details/5
+        // GET: /ChiTieu/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -35,36 +145,41 @@ namespace chitieu.Controllers
             return View(expenditure);
         }
 
-        // GET: /expenditure/Create
+        // GET: /ChiTieu/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /expenditure/Create
+        // POST: /ChiTieu/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="id,datetime,amount,note")] expenditure expenditure)
+        public ActionResult Create(expenditure model)
         {
+            ValidateExpenditure(model);
             if (ModelState.IsValid)
             {
-                db.expenditures.Add(expenditure);
+                model.datetime = DateTime.Today;
+                db.expenditures.Add(model);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(expenditure);
+            return View(model);
+        }
+        public void ValidateExpenditure(expenditure model)
+        {
+            if (model.amount <= 0)
+            {
+                ModelState.AddModelError("amount", "Số tiền quá ít");
+            }
         }
 
-        // GET: /expenditure/Edit/5
+        // GET: /ChiTieu/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             expenditure expenditure = db.expenditures.Find(id);
             if (expenditure == null)
             {
@@ -73,23 +188,24 @@ namespace chitieu.Controllers
             return View(expenditure);
         }
 
-        // POST: /expenditure/Edit/5
+        // POST: /ChiTieu/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="id,datetime,amount,note")] expenditure expenditure)
+        public ActionResult Edit([Bind(Include = "amount,id,note,datetime")] expenditure model)
         {
+            ValidateExpenditure(model);
             if (ModelState.IsValid)
             {
-                db.Entry(expenditure).State = EntityState.Modified;
+                db.Entry(model).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(expenditure);
+            return View(model);
         }
 
-        // GET: /expenditure/Delete/5
+        // GET: /ChiTieu/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -104,7 +220,7 @@ namespace chitieu.Controllers
             return View(expenditure);
         }
 
-        // POST: /expenditure/Delete/5
+        // POST: /ChiTieu/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -125,3 +241,4 @@ namespace chitieu.Controllers
         }
     }
 }
+
